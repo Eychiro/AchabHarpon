ed7b9c867b6b341704fd7792d373f888c79b62c5
Wimod <bosc.william31500@gmail.com>
2025-12-07 19:57:09 +0100

    lancer du harpon

--- AchabHapron/Assets/William/Script/disparitionHarpon.cs
+++ AchabHapron/Assets/William/Script/disparitionHarpon.cs
@@ -5,13 +5,12 @@ public class disparitionHarpon : MonoBehaviour
 
     void Start()
     {
-        // 1. Disparaît naturellement après 5 secondes si on ne touche rien
-        Destroy(gameObject, 5f); 
+        Destroy(gameObject, 3f); 
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        // 2. Disparaît IMMÉDIATEMENT si on touche n'importe quelle surface
+        if (collision.gameObject.CompareTag("Monster"))
         Destroy(gameObject);
     }
 }
---
+++ AchabHapron/Assets/William/Script/lancerHarpon.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+
+public class LanceurHarpon : MonoBehaviour
+{
+    public GameObject harponPrefab;
+    public float forceTir = 30f;
+    public float tempsRecharge = 2f;
+    
+    private bool dispoTir = true;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && dispoTir)
+        {
+            StartCoroutine(TirerEtRecharger());
+        }
+    }
+
+    IEnumerator TirerEtRecharger()
+    {
+        dispoTir = false;
+
+        GameObject harpon = Instantiate(harponPrefab, transform.position + transform.forward, transform.rotation);
+        
+        Rigidbody rb = harpon.GetComponent<Rigidbody>();
+        rb.linearVelocity = transform.forward * forceTir;
+
+        while (harpon != null)
+        {
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(tempsRecharge);
+
+        dispoTir = true;
+        Debug.Log("Harpon prêt !");
+    }
+}
